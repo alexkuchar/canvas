a6b7e90b08769d10f5c047ccfb7a99fbeec21d78
Alex Kuchar <hello@alexkuchar.com>
2025-12-22 16:26:45 +0100

    feat(api): configure DbContext for PostgreSQL

--- Server/Canvas.Api/Program.cs
+++ Server/Canvas.Api/Program.cs
@@ -1,11 +1,18 @@
+using Canvas.Api.Data;
 using Canvas.Api.Extensions;
 using Canvas.Api.Extensions.Authentication;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddSwaggerDocs();
 builder.Services.AddAuthenticationAndAuthorization(builder.Configuration);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
