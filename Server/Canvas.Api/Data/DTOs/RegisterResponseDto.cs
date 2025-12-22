7ca5b34598f7b3639755978a3ff1a4be9915aef1
Alex Kuchar <hello@alexkuchar.com>
2025-12-22 23:04:19 +0100

    feat(api): add RegisterResponseDto for user registration response

---
+++ Server/Canvas.Api/Data/DTOs/RegisterResponseDto.cs
@@ -0,0 +1,11 @@
+namespace Canvas.Api.Data.DTOs;
+
+public class RegisterResponseDto
+{
+    public Guid Id { get; set; }
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+}
