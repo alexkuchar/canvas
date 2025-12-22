32fbdafa79c8667cca62d97ae16f0142e4dc6745
Alex Kuchar <hello@alexkuchar.com>
2025-12-22 22:31:27 +0100

    feat(api): implement IUserService and UserService for user management operations

---
+++ Server/Canvas.Api/Services/User/IUserService.cs
@@ -0,0 +1,14 @@
+using Canvas.Api.Data.DTOs;
+
+namespace Canvas.Api.Services;
+
+public interface IUserService
+{
+    Task<Data.Entities.User> CreateAsync(RegisterUserDto registerUserDto);
+
+    Task ChangeEmailAsync(Guid id, string email);
+    Task ChangePasswordAsync(Guid id, string password);
+    Task ChangeNameAsync(Guid id, string firstName, string lastName);
+    Task DeactivateAsync(Guid id);
+    Task DeleteAsync(Guid id);
+}
---
+++ Server/Canvas.Api/Services/User/UserService.cs
@@ -0,0 +1,93 @@
+using Canvas.Api.Data;
+using Canvas.Api.Data.DTOs;
+using Canvas.Api.Data.Repositories;
+using Canvas.Api.Data.Entities;
+using Canvas.Api.Services.User.Exceptions;
+
+namespace Canvas.Api.Services;
+
+public class UserService : IUserService
+{
+    private readonly IUserRepository _userRepository;
+    private readonly IPasswordHasher _passwordHasher;
+    private readonly AppDbContext _context;
+
+    public UserService(IUserRepository userRepository, IPasswordHasher passwordHasher, AppDbContext context)
+    {
+        _userRepository = userRepository;
+        _passwordHasher = passwordHasher;
+        _context = context;
+    }
+
+    public async Task ChangeEmailAsync(Guid id, string email)
+    {
+        var user = await _userRepository.GetUserByIdAsync(id) ?? throw new UserNotFoundException(id);
+
+        if (await _userRepository.GetUserByEmailAsync(email) != null) throw new EmailAlreadyInUseException(email);
+
+        user.ChangeEmail(email);
+
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task ChangeNameAsync(Guid id, string firstName, string lastName)
+    {
+        var user = await _userRepository.GetUserByIdAsync(id) ?? throw new UserNotFoundException(id);
+
+        user.ChangeName(firstName, lastName);
+
+        await _userRepository.UpdateUserAsync(user);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task ChangePasswordAsync(Guid id, string password)
+    {
+        var user = await _userRepository.GetUserByIdAsync(id) ?? throw new UserNotFoundException(id);
+
+        var passwordHash = _passwordHasher.Hash(password);
+
+        user.ChangePassword(passwordHash);
+
+        await _userRepository.UpdateUserAsync(user);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<Data.Entities.User> CreateAsync(RegisterUserDto registerUserDto)
+    {
+        var existingUser = await _userRepository.GetUserByEmailAsync(registerUserDto.Email);
+        if (existingUser != null) throw new EmailAlreadyInUseException(registerUserDto.Email);
+
+        var passwordHash = _passwordHasher.Hash(registerUserDto.Password);
+
+        var user = new Data.Entities.User
+        (
+            registerUserDto.FirstName,
+            registerUserDto.LastName,
+            registerUserDto.Email,
+            passwordHash
+        );
+
+        await _userRepository.AddUserAsync(user);
+        await _context.SaveChangesAsync();
+
+        return user;
+    }
+
+    public async Task DeactivateAsync(Guid id)
+    {
+        var user = await _userRepository.GetUserByIdAsync(id) ?? throw new UserNotFoundException(id);
+
+        user.Deactivate();
+
+        await _userRepository.UpdateUserAsync(user);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(Guid id)
+    {
+        var user = await _userRepository.GetUserByIdAsync(id) ?? throw new UserNotFoundException(id);
+
+        await _userRepository.DeleteUserAsync(id);
+        await _context.SaveChangesAsync();
+    }
+}
