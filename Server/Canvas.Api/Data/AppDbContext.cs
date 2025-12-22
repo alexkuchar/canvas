ac9a5448e6f3a32d651c930fc7746383617627c5
Alex Kuchar <hello@alexkuchar.com>
2025-12-22 16:26:16 +0100

    feat(api): add database context and connection string configuration

---
+++ Server/Canvas.Api/Data/AppDbContext.cs
@@ -0,0 +1,25 @@
+using Canvas.Api.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Canvas.Api.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<User> Users => Set<User>();
+    public DbSet<Session> Sessions => Set<Session>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>()
+            .HasIndex(u => u.Email)
+            .IsUnique();
+
+        modelBuilder.Entity<Session>()
+            .HasIndex(s => s.Token)
+            .IsUnique();
+    }
+}
