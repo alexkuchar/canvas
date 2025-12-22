d899e084fab0b5ac6a34cc640f6e2cf7581c21ff
Alex Kuchar <hello@alexkuchar.com>
2025-12-22 23:04:45 +0100

    feat(api): register services and exception handling middleware in Program.cs

--- Server/Canvas.Api/Program.cs
+++ Server/Canvas.Api/Program.cs
@@ -1,8 +1,11 @@
 using Canvas.Api.Data;
 using Canvas.Api.Extensions;
 using Canvas.Api.Extensions.Authentication;
+using Canvas.Api.Services;
+using Canvas.Api.Data.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
+using Canvas.Api.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,12 +13,22 @@ builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 builder.Services.AddAuthenticationAndAuthorization(builder.Configuration);
 
+builder.Services.AddScoped<ITokenService, TokenService>();
+builder.Services.AddScoped<IPasswordHasher, PasswordHasher>();
+
+builder.Services.AddScoped<IUserService, UserService>();
+
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IAuthRepository, AuthRepository>();
+
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
 var app = builder.Build();
+
+app.UseMiddleware<ExceptionHandlingMiddleware>();
 
 if (app.Environment.IsDevelopment())
 {
