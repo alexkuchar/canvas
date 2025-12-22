6b7a8d2fc555749a0e2fcb863f81ae11bbd4ecef
Alex Kuchar <hello@alexkuchar.com>
2025-12-22 16:30:40 +0100

    chore(api): replace Swagger with Scalar for API documentation and remove related extension methods

--- Server/Canvas.Api/Program.cs
+++ Server/Canvas.Api/Program.cs
@@ -2,11 +2,12 @@ using Canvas.Api.Data;
 using Canvas.Api.Extensions;
 using Canvas.Api.Extensions.Authentication;
 using Microsoft.EntityFrameworkCore;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-builder.Services.AddSwaggerDocs();
+builder.Services.AddOpenApi();
 builder.Services.AddAuthenticationAndAuthorization(builder.Configuration);
 
 builder.Services.AddDbContext<AppDbContext>(options =>
@@ -18,7 +19,7 @@ var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwaggerDocs();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
