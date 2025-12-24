d73455d9d11417a1bb5f8489d437f3de2491ea00
Alex Kuchar <hello@alexkuchar.com>
2025-12-24 16:31:49 +0100

    feat(infrastructure): add migration for verification token type column

---
+++ Server/Canvas.Infrastructure/Migrations/20251224151242_AddTokenTypeColToVerificationTokens.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Canvas.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTokenTypeColToVerificationTokens : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "VerificationTokenType",
+                table: "VerificationTokens",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "VerificationTokenType",
+                table: "VerificationTokens");
+        }
+    }
+}
