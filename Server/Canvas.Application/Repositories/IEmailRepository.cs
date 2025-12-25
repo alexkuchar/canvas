2b32d20a9efa8e2f09b6d18bfb21b00cdc996a72
Alex Kuchar <hello@alexkuchar.com>
2025-12-25 15:06:35 +0100

    feat(application): add welcome and password changed email methods to email repository

--- Server/Canvas.Application/Repositories/IEmailRepository.cs
+++ Server/Canvas.Application/Repositories/IEmailRepository.cs
@@ -3,5 +3,7 @@ namespace Canvas.Application.Repositories;
 public interface IEmailRepository
 {
     Task SendVerificationEmailAsync(string email, string firstName, string verificationLink);
+    Task SendWelcomeEmailAsync(string email, string firstName);
     Task SendPasswordResetEmailAsync(string email, string firstName, string passwordResetLink);
+    Task SendPasswordChangedEmailAsync(string email, string firstName, DateTime timestamp);
 }
