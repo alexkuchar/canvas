0c57280aca52d42ff9e011847c220525be67e6a6
Alex Kuchar <hello@alexkuchar.com>
2025-12-26 19:25:29 +0100

    chore: replace angular with svelte

--- Client/web/app/routes.ts
+++
@@ -1,8 +0,0 @@
-import { type RouteConfig, index, route } from "@react-router/dev/routes";
-
-export default [
-  index("routes/home.tsx"),
-  route("register", "routes/auth/register.tsx"),
-  route("login", "routes/auth/login.tsx"),
-  route("verify-email", "routes/auth/verification-notice.tsx"),
-] satisfies RouteConfig;
