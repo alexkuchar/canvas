953e8c01b2ff3ffae0034dc1f17fc20cd7a02d74
Alex Kuchar <hello@alexkuchar.com>
2025-12-26 13:06:52 +0100

    feat(web): add UI components and authentication form components

---
+++ Client/web/app/components/forms/auth/loginForm.tsx
@@ -0,0 +1,131 @@
+import { useForm } from "@tanstack/react-form";
+import * as z from "zod";
+import axios from "axios";
+import { Button } from "~/components/ui/button";
+import {
+  Field,
+  FieldError,
+  FieldGroup,
+  FieldLabel,
+} from "~/components/ui/field";
+import { Input } from "~/components/ui/input";
+import { toast } from "sonner";
+import { AxiosError } from "axios";
+import { useNavigate } from "react-router";
+import { Spinner } from "~/components/ui/spinner";
+
+const formSchema = z.object({
+  email: z.email({ message: "Invalid email address" }),
+  password: z
+    .string()
+    .min(8, { message: "Password must be at least 8 characters long" }),
+});
+
+const LoginForm = () => {
+  const navigate = useNavigate();
+
+  const form = useForm({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+    validators: {
+      onChange: formSchema,
+      onSubmit: formSchema,
+    },
+    onSubmit: async (values) => {
+      await axios
+        .post("http://localhost:5163/api/Auth/login", {
+          email: values.value.email,
+          password: values.value.password,
+        })
+        .then((response) => {
+          toast.success("Login successful");
+          console.log(response);
+          form.reset();
+          navigate("/verify-email");
+        })
+        .catch((error) => {
+          const err = error as AxiosError;
+          toast.error(
+            (err.response?.data as { error: { message: string } }).error.message
+          );
+          throw error;
+        });
+    },
+  });
+  return (
+    <form
+      id="login-form"
+      onSubmit={(e) => {
+        e.preventDefault();
+        form.handleSubmit();
+      }}
+    >
+      <FieldGroup>
+        <form.Field
+          name="email"
+          children={(field) => {
+            const isInvalid =
+              field.state.meta.isTouched && !field.state.meta.isValid;
+
+            return (
+              <Field data-invalid={isInvalid}>
+                <FieldLabel htmlFor={field.name}>Email</FieldLabel>
+                <Input
+                  type="email"
+                  id={field.name}
+                  name={field.name}
+                  value={field.state.value}
+                  onBlur={field.handleBlur}
+                  onChange={(e) => field.handleChange(e.target.value)}
+                />
+                {isInvalid && <FieldError errors={field.state.meta.errors} />}
+              </Field>
+            );
+          }}
+        />
+        <form.Field
+          name="password"
+          children={(field) => {
+            const isInvalid =
+              field.state.meta.isTouched && !field.state.meta.isValid;
+
+            return (
+              <Field data-invalid={isInvalid}>
+                <FieldLabel htmlFor={field.name}>Password</FieldLabel>
+                <Input
+                  type="password"
+                  id={field.name}
+                  name={field.name}
+                  value={field.state.value}
+                  onBlur={field.handleBlur}
+                  onChange={(e) => field.handleChange(e.target.value)}
+                />
+                {isInvalid && <FieldError errors={field.state.meta.errors} />}
+              </Field>
+            );
+          }}
+        />
+
+        <form.Subscribe
+          selector={(state) => ({
+            isValid: state.isValid,
+            isSubmitting: state.isSubmitting,
+          })}
+        >
+          {(state) => (
+            <Button
+              type="submit"
+              disabled={!state.isValid || state.isSubmitting}
+            >
+              {state.isSubmitting ? <Spinner /> : "Continue"}
+            </Button>
+          )}
+        </form.Subscribe>
+      </FieldGroup>
+    </form>
+  );
+};
+
+export default LoginForm;
