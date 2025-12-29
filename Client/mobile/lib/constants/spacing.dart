0074f117a3011913e46ca287211db1f6a70dd6b2
Alex Kuchar <hello@alexkuchar.com>
2025-12-29 16:15:27 +0100

    feat(mobile): add spacing constants

---
+++ Client/mobile/lib/constants/spacing.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppSpacing {
+  AppSpacing._();
+
+  static const double paddingDefault = 24.0;
+
+  static const EdgeInsets paddingDefaultAll = EdgeInsets.all(paddingDefault);
+
+  static const double spacingDefault = 24.0;
+  static const double spacingSmall = 12.0;
+}
