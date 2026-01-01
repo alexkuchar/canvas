59a91ee8002429397fa9bae245c80face030c8c6
Alex Kuchar <hello@alexkuchar.com>
2026-01-01 14:08:11 +0100

    feat(mobile): enhance dashboard with welcome message and user name

--- Client/mobile/lib/screens/dashboard/overview.dart
+++ Client/mobile/lib/screens/dashboard/overview.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:mobile/constants/spacing.dart';
+import 'package:mobile/services/storage_service.dart';
 
 class DashboardOverviewScreen extends StatelessWidget {
   const DashboardOverviewScreen({super.key});
@@ -15,7 +17,33 @@ class DashboardOverviewScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: const Center(child: Text('Dashboard')),
+      body: Center(
+        child: Padding(
+          padding: AppSpacing.paddingDefaultAll,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                "Welcome back,",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+              ),
+              FutureBuilder<String?>(
+                future: StorageService.getFullName(),
+                builder: (context, snapshot) {
+                  final fullName = snapshot.data ?? 'Unknown User';
+                  return Text(
+                    "$fullName!",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.w500),
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
